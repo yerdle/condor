111be8f958e70ee2d5291d031acc4aff733866ec
Joshua Morris <joshua@yerdle.com>
2014-09-19 10:21:29 -0700

    DSL class namespacing fixes

--- lib/condor/dsl/closure.rb
+++ lib/condor/dsl/closure.rb
@@ -1,32 +1,34 @@
 require 'active_support/core_ext/string'
 
 module Condor
-  class Closure
-    class ContextRequiredError < RuntimeError; end;
+  module DSL
+    class Closure
+      class ContextRequiredError < RuntimeError; end;
 
-    attr_reader :enclosure, :options
+      attr_reader :enclosure, :options
 
-    def initialize(enclosure, options={})
-      @enclosure = enclosure
-      @options   = options
-    end
+      def initialize(enclosure, options={})
+        @enclosure = enclosure
+        @options   = options
+      end
 
-    # accessors via closure inheritance
+      # accessors via closure inheritance
 
-    def event_registry
-      options[:event_registry] || enclosure.event_registry
-    end
+      def event_registry
+        options[:event_registry] || enclosure.event_registry
+      end
 
-    def event_name
-      options[:event_name] || enclosure.event_name
-    rescue NoMethodError
-      raise ContextRequiredError, :event_name
-    end
+      def event_name
+        options[:event_name] || enclosure.event_name
+      rescue NoMethodError
+        raise ContextRequiredError, :event_name
+      end
 
-    def event_domain
-      options[:event_domain] || enclosure.event_domain
-    rescue NoMethodError
-      raise ContextRequiredError, :event_domain
+      def event_domain
+        options[:event_domain] || enclosure.event_domain
+      rescue NoMethodError
+        raise ContextRequiredError, :event_domain
+      end
     end
   end
 end
--- lib/condor/dsl/runner.rb
+++ lib/condor/dsl/runner.rb
@@ -1,25 +1,27 @@
 module Condor
-  class Runner < Object
-    attr_reader :closure, :dsl
+  module DSL
+    class Runner < Object
+      attr_reader :closure, :dsl
 
-    def initialize(closure, dsl)
-      @closure = closure
-      eigenclass = (class << self; self; end)
-      eigenclass.send(:include, dsl)
-    end
-
-    def eval(&block)
-      unless block_given?
-        raise ArgumentError, 'block must be present'
+      def initialize(closure, dsl)
+        @closure = closure
+        eigenclass = (class << self; self; end)
+        eigenclass.send(:include, dsl)
       end
-      instance_eval(&block)
-    end
 
-    def parse(source)
-      unless source.respond_to?(:to_str)
-        raise ArgumentError, 'source must respond to #to_str'
+      def eval(&block)
+        unless block_given?
+          raise ArgumentError, 'block must be present'
+        end
+        instance_eval(&block)
       end
-      instance_eval(source.to_str)
+
+      def parse(source)
+        unless source.respond_to?(:to_str)
+          raise ArgumentError, 'source must respond to #to_str'
+        end
+        instance_eval(source.to_str)
+      end
     end
   end
 end
