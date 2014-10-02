be18bfffc936302c6622baa0cf1eb5834c4357f9
Joshua Morris <joshua@yerdle.com>
2014-10-02 15:52:09 -0700

    keen relay

--- lib/condor/relay/keen.rb
+++ lib/condor/relay/keen.rb
@@ -10,6 +10,13 @@ module Condor
       def initialize(client)
         @client = client
       end
+
+      def publish(event, event_data)
+        event_data.each do |domain, domain_data|
+          transform = Keen.transform(domain_data, event)
+          client.publish(domain, transform)
+        end
+      end
     end
   end
 end
--- spec/condor/relay/keen_spec.rb
+++ spec/condor/relay/keen_spec.rb
@@ -3,6 +3,25 @@ require 'spec_helper'
 module Condor
   module Relay
     describe Keen do
+      let(:fake_client) { double('keen client').as_null_object }
+
+      subject { Keen.new(fake_client) }
+
+      describe '#publish' do
+        let(:event)      { :signup }
+        let(:event_data) { { board: { is_first_time: true } } }
+
+        it 'transforms input' do
+          expect(described_class).to receive(:transform).and_call_original
+          subject.publish(:signup, event_data)
+        end
+
+        it 'publishes transformed input' do
+          expect(described_class).to receive(:transform).once.ordered
+          expect(fake_client).to receive(:publish).once.ordered
+          subject.publish(:signup, event_data)
+        end
+      end
     end
   end
 end
